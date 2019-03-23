e56e63cb3f362ef55d1a4b8c0577ba9e23cea22a
= <=>
2019-03-23 10:08:04 +0530

    remove files

--- com/bridgeLab/dataStructure/calendar/Month.java
+++ com/bridgeLab/dataStructure/calendar/Month.java
@@ -5,24 +5,21 @@ import com.bridgelab.utility.Utility;
 public class Month {
 
 	String month[][] = new String[7][7];
-//	int mo=1;
+
 	int y=2019;
 	int janFirstDay=3;
-//	int emptyBlock;
 	int start=1;
 	int mn[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 	Utility u =new Utility();
 
 	public String[][] monthCreation(int mon,int year) {
 		int day = decision(year);
-//		System.out.println(day);
 		int temp=0;
 		for(int i=0; i<mon-1; i++) {
 			temp = temp+mn[i]%7;
 		}
 		if(u.isLeapYear(year) && mon>2) temp+=1;
 		temp=temp%7+day;
-//		System.out.println(temp);
 		month[0][0]="S";
 		month[0][1]="M";
 		month[0][2]="T";
