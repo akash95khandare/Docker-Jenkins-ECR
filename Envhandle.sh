c0f82e1188dc08258c16d80f42461412d5470030
Akash Khandare <47408002+akash95khandare@users.noreply.github.com>
2019-04-19 18:21:23 +0530

    Update Envhandle.sh

--- Envhandle.sh
+++ Envhandle.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-source /home/ubuntu/venv/bin/activate
-pip3 install gunicorn
-cd /home/ubuntu/chatapplication/
-pip3 install -r req.txt
-pip3 install django
-pip3 install channels
-pip3 install django-environ
-
+cd /home/ubuntu/chatapplication/;surce /home/ubuntu/venv/bin/activate;pip3 install gunicorn;pip3 install -r req.txt;pip3 install django;pip3 install channels;pip3 install django-environ;
 echo 'DEBUG=TRUE
 # DJANGO_SETTINGS_MODULE=myapp.settings.dev
 SECRET_KEY=xux7u0pd8r#k)fbf6+s4!8uksw(e!-y!je($v_j7m1r+g@z32-
