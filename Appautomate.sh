9a230f5bb307d77242ab729dfe42e24383321775
Akash Khandare <47408002+akash95khandare@users.noreply.github.com>
2019-04-21 21:11:46 +0530

    Update Appautomate.sh

--- Appautomate.sh
+++ Appautomate.sh
@@ -7,7 +7,7 @@ After=network.target
 User=root
 Group=www-data
 WorkingDirectory=/home/ubuntu/chatapplication/
-ExecStart=/usr/bin/gunicorn --workers 3 --bind unix:/home/ubuntu/chatapplication/chatapplication.sock chatapplication.wsgi:application
+ExecStart=/usr/local/bin/gunicorn --workers 3 --bind unix:/home/ubuntu/chatapplication/chatapplication.sock chatapplication.wsgi:application
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/gunicorn.service
 systemctl daemon-reload
